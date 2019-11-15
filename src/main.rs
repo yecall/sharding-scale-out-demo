da327cb5a04601f6c4dfdef68be1df6d0e5c8d01
guoxbin <guoxbin@gmail.com>
2019-11-15 16:55:25 +0800

    Make be like yeeroot

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::cell::RefCell;
 use futures::sync::oneshot;
 use futures::sync::oneshot::Receiver;
 use futures::Stream;
-use futures::future::Future;
+use futures::future::{self, Future};
 use tokio::runtime::Runtime;
 use tokio::runtime::TaskExecutor;
 use exit_future::Exit;
@@ -10,54 +10,79 @@ use tokio::timer::Interval;
 use std::time::{Instant, Duration};
 use std::thread::sleep;
 use log::warn;
+use signal_hook::{iterator::Signals, SIGUSR1};
 
 fn main() {
+    execute();
+}
+
+fn execute() {
+
+    let outer_exit = OuterExit;
+
+    start_service(outer_exit);
+
+}
+
+fn start_service(outer_exit: OuterExit){
+
+    println!("Start service");
 
     let mut runtime = Runtime::new().map_err(|e| format!("{:?}", e)).expect("qed");
     let executor = runtime.executor();
 
-    let ctrlc_exit = get_ctrlc_exit();
-
     let (exit_send, exit) = exit_future::signal();
 
-    start_service(exit, executor);
+    start_task(exit, executor);
 
-    runtime.block_on(ctrlc_exit).expect("qed");
+    runtime.block_on(outer_exit.into_exit());
 
     exit_send.fire();
 
-    println!("End");
-
+    println!("Finish service");
 }
 
-fn get_ctrlc_exit() -> Receiver<()>{
+fn start_task(exit: Exit, executor: TaskExecutor){
 
-    let (ctrlc_send, ctrlc_exit) = oneshot::channel();
-    let ctrlc_send = RefCell::new(Some(ctrlc_send));
-    ctrlc::set_handler(move || {
-        println!("Ctrl+C");
-        ctrlc_send.borrow_mut().take().unwrap().send(()).expect("Error sending exit notification");
-    }).expect("Error setting Ctrl-C handler");
-
-
-    ctrlc_exit
-}
-
-fn start_service(exit: Exit, executor: TaskExecutor){
-
-    println!("Start service");
+    println!("Start task");
 
     let task = Interval::new(Instant::now(), Duration::from_secs(10)).for_each(move |_instant| {
 
-        println!("loop start");
+        println!("task loop start");
 
         sleep(Duration::from_secs(3));
 
-        println!("loop finish");
+        println!("task loop finish");
 
         Ok(())
     }).map_err(|e| warn!("Error: {:?}", e));
 
     executor.spawn(exit.until(task).map(|_| ()));
 
+    println!("Finish task");
+}
+
+pub trait IntoExit {
+    /// Exit signal type.
+    type Exit: Future<Item=(),Error=()> + Send + 'static;
+    /// Convert into exit signal.
+    fn into_exit(self) -> Self::Exit;
+}
+
+pub struct OuterExit;
+impl IntoExit for OuterExit {
+    type Exit = future::MapErr<oneshot::Receiver<()>, fn(oneshot::Canceled) -> ()>;
+    fn into_exit(self) -> Self::Exit {
+        // can't use signal directly here because CtrlC takes only `Fn`.
+        let (exit_send, exit) = oneshot::channel();
+
+        let exit_send_cell = RefCell::new(Some(exit_send));
+        ctrlc::set_handler(move || {
+            if let Some(exit_send) = exit_send_cell.try_borrow_mut().expect("signal handler not reentrant; qed").take() {
+                exit_send.send(()).expect("Error sending exit notification");
+            }
+        }).expect("Error setting Ctrl-C handler");
+
+        exit.map_err(drop)
+    }
 }
