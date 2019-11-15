bf36f5646f3bda70f94b60f2cb79389dda5a192f
guoxbin <guoxbin@gmail.com>
2019-11-15 20:31:27 +0800

    Make service restartable

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use std::cell::RefCell;
 use futures::sync::oneshot;
-use futures::sync::oneshot::Receiver;
+use futures::sync::oneshot::{Sender};
 use futures::Stream;
 use futures::future::{self, Future};
 use tokio::runtime::Runtime;
 use tokio::runtime::TaskExecutor;
 use exit_future::Exit;
-use tokio::timer::Interval;
+use tokio::timer::{Interval, Delay};
 use std::time::{Instant, Duration};
 use std::thread::sleep;
 use log::warn;
 use signal_hook::{iterator::Signals, SIGUSR1, SIGINT, SIGTERM};
 use std::thread;
+use std::sync::{Arc, Mutex};
+use std::fmt::Debug;
 
 fn main() {
 
@@ -34,7 +35,8 @@ fn execute() -> ExitItem {
 
 }
 
-fn start_service<E: IntoExit>(outer_exit: E) -> E::Item{
+fn start_service<E>(outer_exit: E) -> <E::SendExit as SendExit>::Item
+where E : IntoExit<SendExit=OuterSendExit<ExitItem>>{
 
     println!("Start service");
 
@@ -43,9 +45,13 @@ fn start_service<E: IntoExit>(outer_exit: E) -> E::Item{
 
     let (exit_send, exit) = exit_future::signal();
 
-    start_task(exit, executor);
+    let (outer_exit, outer_send_exit) = outer_exit.into_exit();
 
-    let exit_item = runtime.block_on(outer_exit.into_exit()).unwrap();
+    start_restarter::<E>(exit.clone(), &executor, outer_send_exit);
+
+    start_task(exit, &executor);
+
+    let exit_item = runtime.block_on(outer_exit).unwrap();
 
     exit_send.fire();
 
@@ -54,7 +60,28 @@ fn start_service<E: IntoExit>(outer_exit: E) -> E::Item{
     exit_item
 }
 
-fn start_task(exit: Exit, executor: TaskExecutor){
+fn start_restarter<E: IntoExit>(exit: Exit, executor: &TaskExecutor, outer_send_exit: E::SendExit)
+    where E : IntoExit<SendExit=OuterSendExit<ExitItem>>{
+
+    println!("Start restarter");
+
+    let task = Delay::new(Instant::now() + Duration::from_secs(15)).then(move |_x| {
+
+        println!();
+        println!("Restart");
+
+        outer_send_exit.send(ExitItem::Restart);
+
+        Ok(())
+    });
+
+    executor.spawn(exit.until(task).map(|_| ()));
+
+    println!("Finish restarter");
+
+}
+
+fn start_task(exit: Exit, executor: &TaskExecutor){
 
     println!("Start task");
 
@@ -71,15 +98,19 @@ fn start_task(exit: Exit, executor: TaskExecutor){
 
     executor.spawn(exit.until(task).map(|_| ()));
 
-    println!("Finish task");
+}
+
+pub trait SendExit{
+    type Item: Send + 'static;
+    fn send(&self, item: Self::Item);
 }
 
 pub trait IntoExit {
-    type Item: Send + 'static;
+    type SendExit: SendExit + Send + 'static;
     /// Exit signal type.
-    type Exit: Future<Item=Self::Item,Error=()> + Send + 'static;
+    type Exit: Future<Item=<Self::SendExit as SendExit>::Item,Error=()> + Send + 'static;
     /// Convert into exit signal.
-    fn into_exit(self) -> Self::Exit;
+    fn into_exit(self) -> (Self::Exit, Self::SendExit);
 }
 
 #[derive(Debug)]
@@ -88,15 +119,30 @@ pub enum ExitItem{
     Restart,
 }
 
+pub struct OuterSendExit<Item>{
+    sender: Arc<Mutex<Option<Sender<Item>>>>,
+}
+
+impl<Item: Send + Debug + 'static> SendExit for OuterSendExit<Item> {
+    type Item = Item;
+    fn send(&self, item: Self::Item){
+        if let Some(exit_send) = self.sender.lock().unwrap().take() {
+            exit_send.send(item).expect("Error sending exit notification");
+        }
+    }
+}
+
 pub struct OuterExit;
 impl IntoExit for OuterExit {
-    type Item = ExitItem;
-    type Exit = future::MapErr<oneshot::Receiver<Self::Item>, fn(oneshot::Canceled) -> ()>;
-    fn into_exit(self) -> Self::Exit {
+    type SendExit = OuterSendExit<ExitItem>;
+    type Exit = future::MapErr<oneshot::Receiver<<Self::SendExit as SendExit>::Item>, fn(oneshot::Canceled) -> ()>;
+    fn into_exit(self) -> (Self::Exit, Self::SendExit) {
         // can't use signal directly here because CtrlC takes only `Fn`.
         let (exit_send, exit) = oneshot::channel();
 
-        let exit_send_cell = RefCell::new(Some(exit_send));
+        let exit_send_cell = Arc::new(Mutex::new(Some(exit_send)));
+
+        let exit_send_cell_clone = exit_send_cell.clone();
 
         let signals = Signals::new(&[SIGUSR1, SIGINT, SIGTERM]).unwrap();
 
@@ -109,12 +155,12 @@ impl IntoExit for OuterExit {
                     _ => ExitItem::Stop,
                 };
 
-                if let Some(exit_send) = exit_send_cell.try_borrow_mut().expect("signal handler not reentrant; qed").take() {
+                if let Some(exit_send) = exit_send_cell.lock().unwrap().take() {
                     exit_send.send(item).expect("Error sending exit notification");
                 }
             }
         });
 
-        exit.map_err(drop)
+        (exit.map_err(drop), OuterSendExit{sender: exit_send_cell_clone})
     }
 }
